01b70e959035f964e694321b5558204d917e24ae
Matt Lopez <matthew.lopez4011@gmail.com>
2020-03-29 14:54:13 -0500

    add fragment to app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import React, {useState, useEffect} from "react";
 import './index.css';
-import {Meme} from "./Meme";
+import {Meme} from "./components/Meme";
 
 function App() {
-    const [templates, setTemplates] = useState([]);
-    const [template, setTemplate] = useState(null);
+  const [templates, setTemplates] = useState([]);
+  const [template, setTemplate] = useState(null);
 
-    useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(x => x.json()
-            .then(response => setTemplates(response.data.memes))
-            );
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(x => x.json()
+        .then(response => setTemplates(response.data.memes))
+      );
 
-    }, []);
+  }, []);
 
-    return (
-        <div style={{textAlign: "center"}}>
-            {/*Show the meme clicked*/}
-            {template && <Meme template={template}/> }
-        {/*  Meaning behind "!template &&". If they have not chosen a template show them all the options. */}
-        {!template && templates.map(template => {
+  return (
+    <div style={{textAlign: "center"}}>
+      {/*Show the meme clicked*/}
+      {template && <Meme template={template}/>}
+      {/*  Meaning behind "!template &&". If they have not chosen a template show them all the options. */}
+      {!template && (
+        <>
+          <h1>Pick a template</h1>
+          {templates.map(template => {
             return (
-                <Meme
+              <Meme
                 template={template}
                 onClick={() => {
-                    setTemplate(template)
-                }
-                }/>
-            )
-        })}
+                  setTemplate(template);
+                }}
+              />
+            );
+          })}
+        </>
+      )}
     </div>
-    );
+  );
 }
 
-export default App;
+
+  export default App;
